8ce6bb5f09baf33e5b9be39773c86d388c6ee42a
Cody <41700091+codysork@users.noreply.github.com>
2020-04-26 02:05:57 -0700

    Update config.h

--- src/config.h
+++ src/config.h
@@ -6,7 +6,7 @@ static unsigned int cols = 80;
 static unsigned int rows = 24;
 
 /* ref: http://freedesktop.org/software/fontconfig/fontconfig-user.html */
-static char *font = "Source Code Pro:size=13:antialias=true:autohint=true;";
+static char *font = "Source Code Pro:pixelsize=30:antialias=true:autohint=true;";
 
 /* exec precedence: -e arg, utmp option, SHELL env var, /etc/passwd shell */
 static char *shell = "\0";
