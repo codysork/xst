9a1e91d8a40a991b497e475d56710c5a638b6d68
Cody <41700091+codysork@users.noreply.github.com>
2020-04-26 01:53:31 -0700

    Update config.h

--- src/config.h
+++ src/config.h
@@ -6,7 +6,7 @@ static unsigned int cols = 80;
 static unsigned int rows = 24;
 
 /* ref: http://freedesktop.org/software/fontconfig/fontconfig-user.html */
-static char *font = "SauceCodePro Nerd Font Mono:pixelsize=14:antialias=true:autohint=true;";
+static char *font = "SauceCodePro Nerd Font Mono:pixelsize=16:antialias=true:autohint=true;";
 
 /* exec precedence: -e arg, utmp option, SHELL env var, /etc/passwd shell */
 static char *shell = "\0";
